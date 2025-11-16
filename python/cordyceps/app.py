7adcf4995ab82f523b5d67a1672c4dd660fd2635
ye11owSub <exactlythatguy@gmail.com>
2025-11-16 22:18:01 +0000

    adding basic tringle shader

--- python/cordyceps/app.py
+++ python/cordyceps/app.py
@@ -4,5 +4,8 @@ from PyQt6.QtWidgets import QApplication
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
+    #with open("style.qss", "r") as f:
+    #    _style = f.read()
+    #    app.setStyleSheet(_style)
     window.show()
     app.exec()
